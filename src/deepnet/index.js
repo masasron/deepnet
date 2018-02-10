819b7fb50eb76eb6bfc0a411d5bd25b733d90a80
Ron Masas <ronm@bbinary.com>
2018-02-10 18:20:53 +0200

    Update index.js

--- src/deepnet/index.js
+++ src/deepnet/index.js
@@ -1,11 +1,5 @@
 'use strict'
 
-const fs = require('fs')
-
-const shuffle = require('./shuffle')
-const console2 = require('./console2')
-const vectorize = require('./vectorize')
-
 const TrainCommand = require('./commands/train')
 const PredictCommand = require('./commands/predict')
 const MakeDatasetCommand = require('./commands/make-dataset')
